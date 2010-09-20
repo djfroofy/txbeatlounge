239f8cbc4ffb1e26905c2d504443daa28ed996f8
Skylar Saveland <skylar.saveland@transgaming.com>
2010-09-19 21:05:50 -0400

    some fixes and betterness

--- comp2.py
+++ comp2.py
@@ -11,12 +11,12 @@ import constants
 
 
 e = Enunciator(sf2path='sf2/Ob-3.sf2')
-e2 = Enunciator(sf2path='sf2/english_organ.sf2', channel=1)
-e3 = Enunciator(sf2path='sf2/hs_magic_techno_drums.sf2', channel=2)
+e2 = Enunciator(sf2path='sf2/english_organ.sf2') # channel=1 (less staccato)
+e3 = Enunciator(sf2path='sf2/hs_magic_techno_drums.sf2') # channel=2
 
 p = PatternGenerator(e)
 p2 = PatternGenerator(e2, noteweights=[('B', 8), ('F', 4), ('A', 8), ('D', 12), ('C', 10), ('E', 10), ('G', 10)])
-p3 = PatternGenerator(e3)
+p3 = PatternGenerator(e3, noteweights=constants.all_equal())
 
 pattern = p.get_generator()
 pattern2 = p2.get_generator()
